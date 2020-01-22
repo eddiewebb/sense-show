14637b8eb8fa363bd42020ef1a281cc143db76ef
Eddie Webb <ollitech@gmail.com>
2020-01-21 19:12:45 -0500

    more

--- leds.py
+++ leds.py
@@ -8,7 +8,7 @@ leds.append([15,14])
 
 
 pixels = neopixel.NeoPixel(board.D18, 256)
-for i in range(0,258): # notice we go over!!
+for i in range(0,259): # notice we go over!!
 	if i > 0 and i < 256:
 		pixels[i] = (0,255,0) # green
 	if i > 1  and i-1 < 256:
@@ -17,7 +17,7 @@ for i in range(0,258): # notice we go over!!
 		pixels[i-2] = (0,0,255) #blyue
 	if i > 3  and i-3 < 256:
 		pixels[i-3] = (0,0,0) #off
-	time.sleep(.01)
+	time.sleep(.005)
 
 
 
