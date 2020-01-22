00eb467bed4ab76aa1bd732fb1ad41e32a47ef24
Eddie Webb <ollitech@gmail.com>
2020-01-21 19:00:16 -0500

    more

--- leds.py
+++ leds.py
@@ -1,5 +1,6 @@
 import board
 import neopixel
+import time
 
 leds = list()
 leds.append([0,1])
@@ -7,5 +8,15 @@ leds.append([15,14])
 
 
 pixels = neopixel.NeoPixel(board.D18, 256)
-for i in range(0,255):
-	pixels[i] = (0,0,0)
+for i in range(0,258): # notice we go over!!
+	if i > 0 and i < 256:
+		pixels[i-1] = (0,255,0)
+	if i > 1  and i < 256:
+		pixels[i-2] = (255,0,0)
+	if i > 2  and i < 256:
+		pixels[i-3] = (0,0,0)
+	time.sleep(.1)
+
+
+
+
