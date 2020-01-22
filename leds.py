a0332207b220a35411b78ed719e953707693c5e4
Eddie Webb <ollitech@gmail.com>
2020-01-21 19:27:12 -0500

    more

--- leds.py
+++ leds.py
@@ -2,9 +2,28 @@ import board
 import neopixel
 import time
 
+#
+#. our led is a strip essentially folded every 8 lights, zig zagging
+# so the id at coordinates 0:0 is 0, but id at 1:0 (2nd column, first row) is 15 !
+#
+
+
+
 leds = list()
-leds.append([0,1])
-leds.append([15,14])
+leds.append([0,1,2,3,4,5,6,7])
+leds.append([15,14,13,12,11,10,9,8])
+
+
+def get_id_by_coordinates(x,y):
+	id=0
+	if x > 32 or y > 8:
+		raise("invaliud coordinted, must be < 32:8, coordinates start top left")
+	if x % 2:
+		# even rows count up from bottom
+		id=(x*8)-y
+	else:
+		# odd coordinates (first column, 3rd colunm, count down from top left.)
+		id=((x-1)*8)+(y-1)
 
 
 pixels = neopixel.NeoPixel(board.D18, 256)
@@ -20,5 +39,8 @@ for i in range(0,259): # notice we go over!!
 	time.sleep(.005)
 
 
+for x in range(1,32):
+	for y in range(1,8):
+		pixels[get_id_by_coordinates(x,y)] = (126,126,255)
 
 
