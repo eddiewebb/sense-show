8c501ff8d56f383710747cfd9ef3fd0336746eb0
Eddie Webb <ollitech@gmail.com>
2020-01-24 19:51:06 -0500

    Make leds dance
    This commit adds a real led library neopixel and also a mock one for testing on machines without GPIO.
    
    Since there was essentially no code prior to this commit I willnot enumerate everything. This should be considered a working, alpha version of the software, it may have memory leaks or other issues after long runs, but is stable now in my day long cycles.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Sense Show
+
+Simple python script to visual Sense Energy consumption & solar generation on an LED matrix.
+
+Uses a raspberry 3 and python.
+
+## Running it
+`SENSE_USER="email" SENSE_PASSWD='5ecr!ts' sudo -E python3 main.py`
+
+## Output
+
+stdout will see progess bars showing usage
+
+```
+From Solar:  -0%|          | -7.00/8.00k [00:00<-1:59:59, -7.09kwatts/s]
+Consumption:  12%|█▏        | 1.74k/15.0k [00:00<00:00, 1.40Mwatts/s]
+From Grid:  12%|█▏        | 1.75k/15.0k [00:00<00:00, 1.67Mwatts/s]
+```
+
+## Leds
+
+If wired to an LED strip you'll also visualize it. I bought a cheap serial style one on amazon.
+
+[8x32 LED Panel - Individually Addressable
+![8x32 led panel](https://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B07P5TSKX8&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=eddiewebb-20&language=en_US)](https://amzn.to/2RoNRGH)
+
+### Objects
+- Grid - depicted with tower on left
+- House - depicted with blue house in center
+- Panels - depicted by blue stripes on right
+- Sun - Small yellow dot abov panels
+- Energy Flow Green, Yellow or Red leds flow in the direction of energy, sized base on total watts.
+
+### Overproducing
+
+Orange/Yellow LEDs will flow from panels, through house (reduced by usage) and back to grid. Shown here, these yellow LEDs are moving right (solar) to left (grid)
+
+![Overproducing](/assets/overproduce.jpg)
+
+### Consumption
+
+Red LEDs will flow from grid to house, and feeding panels during shade/night.  For instance here my idle house is using minimum electricity and feeding about 8 watts to panels at night. Lights are moving left (grid) to right( house and panels)
+
+![NIghtly consumption](/assets/night.jpg)
--- main.py
+++ main.py
@@ -1,86 +1,173 @@
 import os
-import sense_energy
+import signal
 import time
-import logging
 import threading
-from tqdm import tqdm, trange
-
+import logging
 from queue import Queue
+from collections import deque
+import sense_energy
+from tqdm import tqdm
 
+import led_strip
+
+
+max_solar 	   = 8000
+max_use   	   = 15000
+flip_iterations= 5
+data_queue    = Queue()
+led_panel        = None
+threads = list()
+keep_running = True
+
+logging.basicConfig(filename='/var/log/sense-debug.log',level=logging.DEBUG)
+log = logging.getLogger('senseshow.main')
 
 def main():
-	global solar_queue, use_queue
+	signal.signal(signal.SIGINT, exit_gracefully)
+	signal.signal(signal.SIGTERM, exit_gracefully)
 
-	solar_queue = Queue()
-	use_queue = Queue()
-	functions = list()
-	functions.append(print_solar)
-	functions.append(print_use)
-	threads = list()
-	for function in functions:
+
+
+	while keep_running:	
+		try:
+			launchAndWait()
+		except KeyboardInterrupt:
+			exit_gracefully()
+			#break
+		except:
+			# any other exception cshould trigger full restart
+			log.exception("Exception in main thread, asking others to close for restart.")
+			time.sleep(5) #error here could be from initializing leds or authing with sense, back off high level ,restart it all again
+			#continue
+	log.info("all done")
+
+def launchAndWait():
+	global led_panel, abort_threads, threads
+
+	log.info("Launching threads")
+	
+	#wait for all previous threads to die
+	halt_threads() 
+	abort_threads = False #let new ones run
+	# discard any poison pills our led will try to eat
+	while data_queue.qsize() > 0:
+		data_queue.get()
+		data_queue.task_done()
+	
+	# start brand new!
+	led_panel = led_strip.LedStrip()
+	led_panel.draw_house()
+	led_panel.draw_panels()
+	led_panel.draw_grid()
+
+	for function in [update_sense_data, update_led_panel]:
 		logging.info("Main    : create and start thread %s.", function)
 		x = threading.Thread(target=function, args=())
 		threads.append(x)
 		x.start()
 
-	while 1:	
-		try:
-			update_sense_data()
-		except KeyboardInterrupt:
-			solar_queue.put(None)
-			use_queue.put(None)
-			exit()
-		except Exception as ex:
-			tqdm.write("Exception encountered.")
-			print(ex)
-			continue
 
-
+	# this is where logic for healthy run is waiting.
 	for index, thread in enumerate(threads):
 		logging.info("Main    : before joining thread %d.", index)
 		thread.join()
 		logging.info("Main    : thread %d done", index)
 
 
+
+
+def halt_threads():
+	global data_queue, abort_threads, threads
+	log.info("Halting threads")
+	# sense and non-blocking threas should respect this.
+	abort_threads = True
+	# q based threads need a poison pill since they block on empty queues.
+	data_queue.put(None)
+
+
+	
+
+def exit_gracefully(signum, frame):
+	global keep_running, led_panel
+	log.info("Shutting down")
+	keep_running = False
+	halt_threads()
+	time.sleep(1)	
+	led_panel.clear()
+
+
+
 def update_sense_data():
-	global use_queue, solar_queue
+	global data_queue, led_panel
 	user = os.getenv("SENSE_USER")
 	passwd = os.getenv("SENSE_PASSWD")	
 	sense = sense_energy.Senseable()
+	sense.rate_limit=10
 	sense.authenticate(user,passwd)
-	fails=0
-	for data in sense.get_realtime_stream():
-		try:
-			stats = dict()
-			stats['from_grid']=data['grid_w']
-			stats['from_solar']=data['d_solar_w']
-			stats['use']=data['d_w']
-			solar_queue.put(stats)
-			use_queue.put(stats)
-			time.sleep(1)
-		except Exception as ex:
-			print(ex)
+	while True:
+		global abort_threads
+		if abort_threads:
+			log.info("Abort threads true, exiting Sense loop")
+			break
+		sense.update_realtime()
+		data = sense.get_realtime()
+		log.debug("Latest Data From: {}".format(time.ctime(data['epoch'])))
+		#qDepth = solar_queue.qsize() + data_queue.qsize()
+		#if qDepth > 0:
+		#	tqdm.write("Queuedepth: " + str(qDepth))
+		data_queue.put(data)
+		time.sleep(1)
 
-def print_solar():
-	global solar_queue
-	t = tqdm(total=15000, unit="watts",miniters=1, position=1, unit_scale=True, leave=True)
-	while 1:
-		data = solar_queue.get()
-		t.total = data['use']
-		t.reset()
-		t.update(data['from_solar'])
-		t.refresh()
+def update_led_panel():
+	log.debug("led function")
+	global data_queue, led_panel
+	solar = tqdm(total=max_solar, unit="watts",desc="From Solar", miniters=1, position=0, unit_scale=True, leave=True)
+	use = tqdm(total=max_use, unit="watts",desc="Consumption",miniters=1, position=1, unit_scale=True, leave=True)
+	grid = tqdm(total=max_use, unit="watts",desc="From Grid",miniters=1, position=2, unit_scale=True, leave=True)
+	while True:
+		while data_queue.qsize() > 5:
+			data_queue.get()
+			data_queue.task_done()
+			log.info("solar discard")
+		data = data_queue.get()
+		# Since queue.get is blocking, we can't use the commong boolean on the loop, instead a None item will kill us.
+		if data == None:
+			#we're being asked to shutdwn
+			log.info("poison pill in queue, exiting LED thread")
+			break
+		else:
+			log.debug("new dtata to show")
+		# Set console indicators	
+		set_tqdm(solar,data['d_solar_w'])
+		set_tqdm(use,data['d_w'])
+		set_tqdm(grid,data['grid_w'])
 
-def print_use():
-	global use_queue
-	t = tqdm(total=15000, unit="watts",miniters=1, position=2, unit_scale=True, leave=True)
-	while 1:
-		data = use_queue.get()
-		t.reset()
-		t.update(data['use'])
-		t.refresh()
+		# flash solar prohress
+		if data['d_solar_w'] < 0:
+			led_panel.show_sun(False)
+			led_panel.flow(19,29, -data['d_solar_w'], max_solar, led_panel.color_red)
+		elif data['d_solar_w'] > 0:
+			led_panel.show_sun(True)
+			led_panel.flow(29,19, data['d_solar_w'], max_solar, led_panel.color_orange)
+
+		#flash grid
+		if data['grid_w'] < 0:
+			led_panel.flow(14,4, -data['grid_w'], max_use, led_panel.color_orange)
+		elif data['grid_w'] > 0:
+			led_panel.flow(4,14, data['grid_w'], max_use, led_panel.color_red)
+	
+		data_queue.task_done()
+	log.debug("led function finishing")
+
+def set_tqdm(instance, new_value):
+		instance.reset()
+		instance.update(new_value)
+		instance.refresh()
 
 
-if __name__ == '__main__':	
 
+
+
+if __name__ == '__main__':
+	print('Sense Show Starting...')
 	main()
