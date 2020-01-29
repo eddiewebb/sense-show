ec5cc8da5c01416dde48746d0c86f98bc8852389
Eddie Webb <ollitech@gmail.com>
2020-01-29 10:15:36 -0500

    more complete install script

--- raspberrypi/install.sh
+++ raspberrypi/install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-sudo apt-get install -y python-smbus
+sudo apt-get install -y python3 python3-pip python3-smbus
 sudo apt-get install -y i2c-tools
 sudo pip3 install --upgrade setuptools
+sudo pip3 install -r ../requirements.txt
+
+# sense websockets dont support TLS 2
+#CipherString = DEFAULT@SECLEVEL=2 --> 1
+
 
 sudo cp senseshow.service /etc/systemd/system/senseshow.service
 
 sudo systemctl enable senseshow.service
+
+
 
 
 ## Notes
