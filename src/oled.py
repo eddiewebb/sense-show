98ae0d7c78dc4fa54014e267f5aea788268934a2
Eddie Webb <ollitech@gmail.com>
2021-02-21 17:00:46 -0500

    new panel image and oled burnin prevention

--- src/oled.py
+++ src/oled.py
@@ -55,7 +55,7 @@ class OLED:
 			# black box to clear every so often
 			log.debug("Blacking screen to prevent burn in")
 			draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
-		if now % 300 == 0:
+		elif now % 300 == 0:
 			# black box to clear every so often
 			log.debug("Whiting screen to prevent burn in")
 			draw.rectangle((0, 0, self.width, self.height), outline=1, fill=1)
